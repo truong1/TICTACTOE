13cc656d7f721ecd28a7edbd1d945a9fe6902664
truong1 <hoanghatvs@gmail.com>
2017-06-27 14:21:20 +0700

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -1,1 +1,18 @@
-goog.provide('tictactoe')
+goog.provide('tictactoe');
+goog.require('lime.Director');
+goog.require('lime.Scene');
+goog.require('lime.Layer');
+goog.require('tic.Land');
+tictactoe.start = function() {
+   var gmObj = {
+   width: 400,
+   height: 500,
+   num_tiles_x: 3,
+   num_tiles_y: 3,
+   landLayer_w: 64*5, 
+   landLayer_h: 64*6,
+   controlsLayer_w: 64*5,
+   controlsLayer_h: 64*1.5,
+   }
+   var director = new lime.Director(document.body,gameObj.width,gameObj.height)
+}
