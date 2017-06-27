5e52397a1d24af5b35a8b9f1433c82f0872ee0d0
truong1 <hoanghatvs@gmail.com>
2017-06-27 22:10:26 +0700

    Update game.js

--- game.js
+++ game.js
@@ -2,5 +2,12 @@ goog.provide('1')
 goog.require('lime.Director')
 game.start() = function() {
   var gameObj = {}
-  var playerObj
-  
+  var playerObj = {
+  money: 0
+  }
+  var listcorrectanswer = []
+  var listquestion = []
+  var listchoiceA = []
+  var listchoiceB = []
+  var listchoiceC = []
+  var listchoiceD = []
