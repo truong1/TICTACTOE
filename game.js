3e1c8f25916551b0e94850c70fcbabbeb3a1b869
truong1 <hoanghatvs@gmail.com>
2017-06-28 10:15:06 +0700

    Update game.js

--- game.js
+++ game.js
@@ -7,6 +7,10 @@ game.start() = function() {
   var playerObj = {
   money: 0
   }
+  var quizComplete = [];
+  var lenCompleteQuiz = quizComplete.length;
+  var money = [0,100,200,300,500,1000,2000,3000,6000,8000,15000,20000,40000,80000,150000]
+  var moneyOfPlayer = 0;
   var listcorrectanswer = [];
   var listquestion = ["1","2"];
   var listchoiceA = [];
